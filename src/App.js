0a8071b07ffde960c8fcdfd5d6c90a08246a66df
PatrikZitting <patrik.zitting@hotmail.fi>
2023-03-01 20:36:30 +0200

    Separated components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import React, { useState } from 'react';
 import './App.css';
 import Todolist from './components/Todolist';
+
 function App() {
-return (
-<div className="App">
-<Todolist />
-</div>
-);
+    const [todo, setTodo] = useState({ desc: "", date: "" });
+    const [todos, setTodos] = useState([]);
+  
+    const addTodo = (event) => {
+      event.preventDefault();
+      setTodos([...todos, todo]);
+      setTodo({ desc: "", date: "" });
+    };
+  
+    const deleteTodo = (index) => {
+      const newTodos = todos.filter((todo, i) => i !== index);
+      setTodos(newTodos);
+    };
+  
+    const inputChanged = (event) => {
+      setTodo({ ...todo, [event.target.name]: event.target.value });
+    };
+  
+    return (
+      <div className="App">
+        <form onSubmit={addTodo}>
+          <input
+            type="text"
+            name="desc"
+            value={todo.desc}
+            onChange={inputChanged}
+          />
+          <input
+            type="date"
+            name="date"
+            value={todo.date}
+            onChange={inputChanged}
+          />
+          <input type="submit" value="Add" />
+        </form>
+        <Todolist todos={todos} deleteTodo={deleteTodo} />
+        </div>
+    );
 }
+
 export default App;
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,58 +1,20 @@
-import React, { useState } from "react";
-import "../App.css";
+import React from 'react';
 
-function Todolist() {
-  const [todo, setTodo] = useState({ desc: "", date: "" });
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (event) => {
-    event.preventDefault();
-    setTodos([...todos, todo]);
-    setTodo({ desc: "", date: "" });
-  };
-
-  const deleteTodo = (index) => {
-    const newTodos = todos.filter((todo, i) => i !== index);
-    setTodos(newTodos);
-  };
-
-  const inputChanged = (event) => {
-    setTodo({ ...todo, [event.target.name]: event.target.value });
-  };
-
-  return (
-    <div className="App">
-      <form onSubmit={addTodo}>
-        <input
-          type="text"
-          name="desc"
-          value={todo.desc}
-          onChange={inputChanged}
-        />
-        <input
-          type="date"
-          name="date"
-          value={todo.date}
-          onChange={inputChanged}
-        />
-        <input type="submit" value="Add" />
-      </form>
+function Todolist(props) {
+  return(
+  <div>
       <table>
         <tbody>
-          <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Action</th>
-          </tr>
-          {todos.map((todo, index) => (
+          {
+          props.todos.map((todo, index) =>
             <tr key={index}>
               <td>{todo.date}</td>
               <td>{todo.desc}</td>
               <td>
-                <button onClick={() => deleteTodo(index)}>Delete</button>
+              <button onClick={() => props.deleteTodo(index)}>Delete</button>
               </td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
